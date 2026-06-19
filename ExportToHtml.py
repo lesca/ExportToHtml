030bc76d6418ccb1146228d61cfa55258381524d
lesca <lesca4on@outlook.com>
2026-06-19 13:55:31 +0800

    feat: support mathjax

--- ExportToHtml.py
+++ ExportToHtml.py
@@ -21,6 +21,7 @@ html_template = """
     <html>
     <head>
         <meta charset="UTF-8">
+        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"></script>
         <style>
         {{style}}
         </style>
